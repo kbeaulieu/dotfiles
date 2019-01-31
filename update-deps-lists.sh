a2b1c494855119f57a524ba2a5485b8198b63763
Keven Beaulieu <beaulieu.keven@gmail.com>
2019-01-31 09:36:00 -0500

    Update global deps

--- update-deps-lists.sh
+++ update-deps-lists.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+echo "🍺 Homebrew"
 brew bundle dump --force
 
+echo "🐻 npm"
 npm list -g > ./global-packages/npm-global.deps.txt
+echo "🧶 yarn"
 yarn global list > ./global-packages/yarn-global.deps.txt
+echo "💎 gem"
 gem list > ./global-packages/gem.deps.txt
+echo "🐍 pip"
 pip list --not-required > ./global-packages/pip.deps.txt
+echo "🐍🐍🐍 pip3"
 pip3 list --not-required > ./global-packages/pip3.deps.txt
