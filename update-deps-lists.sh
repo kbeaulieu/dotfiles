34a0561c1e19692221e0b3c3a715350d87911204
Keven Beaulieu <beaulieu.keven@gmail.com>
2019-01-31 09:20:17 -0500

    Move global packages lists

--- update-deps-lists.sh
+++ update-deps-lists.sh
@@ -2,9 +2,8 @@
 
 brew bundle dump --force
 
-npm list -g > npm-global.deps.txt
-yarn global list > yarn-global.deps.txt
-gem list > gem.deps.txt
-pip list --not-required > pip.deps.txt
-pip3 list --not-required > pip3.deps.txt
-
+npm list -g > ./global-packages/npm-global.deps.txt
+yarn global list > ./global-packages/yarn-global.deps.txt
+gem list > ./global-packages/gem.deps.txt
+pip list --not-required > ./global-packages/pip.deps.txt
+pip3 list --not-required > ./global-packages/pip3.deps.txt
