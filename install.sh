60f388844d92e0d1bf3342381a792744d8ffad2c
Keven Beaulieu <beaulieu.keven@gmail.com>
2019-01-31 10:44:41 -0500

    Merge global deps files

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing brew"
+echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Executing brew bundle"
--- update-deps-lists.sh
+++ update-deps-lists.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
-echo "🍺 Homebrew"
-brew bundle dump --force
+### Homebrew ###
+echo -e "🍺 Homebrew"
+#brew bundle dump --force
 
-echo "🐻 npm"
-npm list -g > ./global-packages/npm-global.deps.txt
-echo "🧶 yarn"
-yarn global list > ./global-packages/yarn-global.deps.txt
-echo "💎 gem"
-gem list > ./global-packages/gem.deps.txt
-echo "🐍 pip"
-pip list --not-required > ./global-packages/pip.deps.txt
-echo "🐍🐍🐍 pip3"
-pip3 list --not-required > ./global-packages/pip3.deps.txt
+### Globals ###
+globalFile=./global-deps.txt
+gnpm="🐻 npm"
+gyarn="🧶  yarn"
+ggem="💎 gem"
+gpip="🐍 pip"
+gpip3="🐍 pip3"
+
+echo -e "\n\n$gnpm"
+echo -e "$gnpm" > $globalFile
+npm list -g >> $globalFile
+
+echo -e "\n\n$gyarn"
+echo -e "\n\n$gyarn" >> $globalFile
+yarn global list >> $globalFile
+
+echo -e "\n\n$ggem"
+echo -e "\n\n$ggem" >> $globalFile
+gem list >> $globalFile
+
+echo -e "\n\n$gpip"
+echo -e "\n\n$gpip" >> $globalFile
+pip list --not-required >> $globalFile
+
+echo -e "\n\n$gpip3"
+echo -e "\n\n$gpip3" >> $globalFile
+pip3 list --not-required >> $globalFile
